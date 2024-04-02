3a7ce7fe0a5fe32cb1dbf026e614e6993c02b707
Ashrafull islam <mmmdashrafull211@gmail.com>
2024-04-02 11:10:48 -0700

    cetagory section

--- Phero.js
+++ Phero.js
@@ -9,10 +9,13 @@ const cetagory=async()=>{
 const displayCategories=(iteam)=>{
     const categoriesId=document.getElementById('Catagories');
     iteam.forEach(iteam => {
-        console.log(iteam.category)
+        // console.log(iteam.category)
     const btn=document.createElement('button');
     btn.classList.add('btn','px-8','btn-active','focus:bg-red-600','text-xl','text-gray-600');
     btn.innerText = iteam.category;
+    btn.onclick =() =>{
+        mainCard(iteam.category_id);
+    }
     categoriesId.appendChild(btn);
     });
 }
@@ -20,6 +23,13 @@ cetagory();
 
 
 // main card section 
-const mainCard=()=>{
+const mainCard= async (categoryId)=>{
+    const LinkCard=await fetch(` https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const res= await LinkCard.json();
+    const data=res.data;
+    Card(data);
+}
+
+const Card=(data)=>{
     
 }
